b607123aa11bd6017a8bcd8cb5a43d64bc70baba
Hannes Winkler <hanneswinkler2000@web.de>
2024-02-19 12:43:25 +0100

    shrink engine checkout

--- dot-gclient.py
+++ dot-gclient.py
@@ -5,6 +5,10 @@ solutions = [{
   "deps_file": "DEPS",
   "custom_vars": {
     "download_linux_deps": True,
-    "download_android_deps": False
+    "download_android_deps": False,
+    "download_esbuild": False,
   },
+  "custom_deps": {
+    "src/flutter/third_party/swiftshader": None,
+  }
 }]
